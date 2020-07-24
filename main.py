fa3b8d26d1749e0edadf3cce11a0c2d9bfc5b1d7
NonagonInfinity <johnson.k@husky.neu.edu>
2020-07-24 19:47:34 -0400

    updated herbo command

--- main.py
+++ main.py
@@ -83,15 +83,15 @@ async def search(ctx, *, arg):
     
 @bot.command(name="list",description="Use the command without an argument to get the categories, then use the command with one of those to get the moves within!", help="e.g. gaia!list Basic Moves", brief="List categories or moves within them")
 async def _list(ctx, *, arg=''): 
-    if arg.lower() not in list(moves_by_source) or arg == '':
+    if arg.title() not in list(moves_by_source) or arg == '':
         response = 'I can display moves from any of the following categories:\n'
         response = response + ', '.join(sorted(list(moves_by_source)))
         await ctx.send(response)
     else:
-        if arg.lower() in list(moves_by_source):
-            names = moves_by_source[arg.lower()]
+        if arg.title() in list(moves_by_source):
+            names = moves_by_source[arg.title()]
             m = discord.Embed()
-            m.title = arg
+            m.title = arg.title()
             m.color = discord.Color.blurple()
             dm_description = ', '.join(list(names))
             m.description = dm_description
@@ -196,7 +196,7 @@ async def greenhouse(ctx):
     
 @bot.command(hidden=True)
 async def herbo(ctx):
-    response = 'Has Boon leaked valuable Big Team secrets again? Unrelated: remind me to look into development of amnestic gases'
+    response = 'Has ~~Boon~~Amaranth leaked valuable ~~Big~~GM Team secrets again? Unrelated: remind me to look into development of amnestic gases'
     await ctx.send(response)
     
 @bot.command(hidden=True)
@@ -453,9 +453,9 @@ async def rise(ctx):
 def sorted_by_source(moves):
     source_dict = {}
     for name, info in moves.items():
-        if info['source'].lower() not in source_dict:
-            source_dict[info['source'].lower()] = []
-        source_dict[info['source'].lower()].append(name)
+        if info['source'] not in source_dict:
+            source_dict[info['source']] = []
+        source_dict[info['source']].append(name)
     return source_dict
 
 def draw_text(img_name, dest, text, x, y):
